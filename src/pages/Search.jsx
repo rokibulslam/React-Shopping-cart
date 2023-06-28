a5c0f83aa438c30e0a84f72dfea99e5acbf0d6a4
rokibulslam <rokibulslam6@gmail.com>
2023-06-29 00:21:06 +0600

    search fix

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -9,6 +9,20 @@ const Search = () => {
   const { data, isError, isFetching, isLoading } = useProductListQuery();
   // Search Functionalities
   let filteredData = data;
+ 
+   if (searchString && filteredData) {
+    filteredData = filteredData?.filter((item) => {
+      const itemName = item.name.toLowerCase();
+      const itemPrice = item.price.toLowerCase();
+      const itemDescription = item.description.toLowerCase();
+      const searchInput = searchString.toLowerCase();
+      return (
+        itemName.includes(searchInput) ||
+        itemPrice.includes(searchInput) ||
+        itemDescription.includes(searchInput)
+      );
+    });
+  }
   let AllData = null;
   if (isLoading || isFetching) {
     AllData = (
@@ -30,19 +44,7 @@ const Search = () => {
       </>
     );
   }
-  if (searchString) {
-    filteredData = filteredData?.filter((item) => {
-      const itemName = item.name.toLowerCase();
-      const itemPrice = item.price.toLowerCase();
-      const itemDescription = item.description.toLowerCase();
-      const searchInput = searchString.toLowerCase();
-      return (
-        itemName.includes(searchInput) ||
-        itemPrice.includes(searchInput) ||
-        itemDescription.includes(searchInput)
-      );
-    });
-  }
+  
   return (
     <div className="space-y-8 mx-10 my-10">
       <h1 className="t text-darkslategray text-[32px] ">Search Data</h1>
